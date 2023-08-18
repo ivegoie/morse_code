2692e4f006a427ffc17f80004f7c36463a44df03
Ivan Živolić <70758717+ivegoie@users.noreply.github.com>
2023-08-18 22:19:35 +0200

    Added copy to clipboard for encoded message

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtGui import QClipboard, QGuiApplication
 from src import Encoder
 
 from src import Ui_MainWindow
@@ -12,12 +13,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.encoder_button.clicked.connect(self.open_encode_frame)
         self.encode_input_button.clicked.connect(self.encode_message)
+        self.copy_encoded_message_button.clicked.connect(self.copy_encoded_message)
 
     def open_encode_frame(self):
         self.morse_buttons_frame.hide()
         self.encoded_frame.show()
+        self.encode_message_frame.hide()
 
     def encode_message(self):
         encoder = Encoder()
+        self.encode_message_frame.show()
         message = self.encode_input_line_edit.text()
         self.encoded_message.setText(encoder.play_morse_code(message))
+
+    def copy_encoded_message(self):
+        clipboard = QGuiApplication.clipboard()
+        clipboard.setText(self.encoded_message.text())
+        self.status_bar.showMessage("Copied to clipboard", 5000)
